0c48924a0eefdd1e16e50a7e30f973b2520b0274
tigami88-beep <tigami88@gmail.com>
2026-05-01 20:46:52 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         int eaters = 5; // сколько людей будут есть
 
-        double water = 3000.0; // миллилитров воды
-        double potatoes = 5.0; // картофелин
-        double chicken = 6.0; // куриных бёдер
-        double spices = 10.0; // ложек специй
-
-        if (eaters < 0) {
-            System.out.println("Число едоков должно быть больше нуля.");
-            return;
-        }
-
-        double waterPerPerson = water / eaters;
-        double potatoesPerPerson = potatoes / (double) eaters;
-        double chickenPerPerson = chicken / (double) eaters;
-        double spicesPerPerson = spices / (double) eaters;
+        int water = 3000; // миллилитров воды
+        int potatoes = 5; // картофелин
+        int chicken = 6; // куриных бёдер
+        int spices = 10; // ложек специй
 
         System.out.println("Сварили суп. На одного человека вышло:");
-        System.out.printf("%.2f миллилитров(а) воды%n", waterPerPerson);
-        System.out.printf("%.2f картофелин(а/ы)%n", potatoesPerPerson);
-        System.out.printf("%.2f куриных(ое) бёдер(ро)%n", chickenPerPerson);
-        System.out.printf("%.2f ложек(ки/ка) специй%n", spicesPerPerson);
+        System.out.println((water / eaters) + " миллилитров(а) воды");
+        System.out.println((potatoes / eaters) + " картофелин(а/ы)");
+        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
+        System.out.println((spices / eaters) + " ложек(ки/ка) специй");
+
     }
 }
